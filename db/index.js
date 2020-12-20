1e20104f002b17f4dcfef6307bf06816912d56ed
Arthur Gomes de Freitas Cardozo <arthurgfcardozo@gmail.com>
2020-12-20 17:48:48 -0300

    postgres data

--- db/index.js
+++ db/index.js
@@ -1,7 +1,4 @@
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('hackinsampa', 'root', '123456', {
-    host: 'localhost',
-    dialect: 'mysql'
-})
+const sequelize = new Sequelize('postgres://bbphuvxyaqpvvk:327fdfbeac4b8ce67c8dc37ff447e992bb1408304ec1dd1a0261ecf2014ea5ab@ec2-100-25-231-126.compute-1.amazonaws.com:5432/dfug8jfed1it6i');
 
 module.exports = {Sequelize, sequelize}
